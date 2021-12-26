235b34f2820ddf7999c8cf8f0d35707a661ed9f1
moisesgithub <moises.github@outlook.com>
2021-12-26 09:50:29 -0300

    Arrays: Criando e Percorrendo um array

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 10;
+arrayInteiros[1] = 20;
+arrayInteiros[2] = 30;
+
+System.Console.WriteLine("Percorrenco o array pelo for");
+
+for (int i = 0; i < arrayInteiros.Length; i++)
+{
+    System.Console.WriteLine(arrayInteiros[i]);
+}
+
+System.Console.WriteLine("Percorrendo o array pelo foreach");
+foreach (var intem in arrayInteiros)
+{
+    System.Console.WriteLine(intem);
+}
