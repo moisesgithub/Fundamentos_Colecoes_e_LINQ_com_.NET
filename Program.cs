a23e38f9b888cdbef7148bc49fe0418537278fcc
moisesgithub <moises.github@outlook.com>
2021-12-27 08:16:25 -0300

    Array: Acessando um array de forma segura

--- Program.cs
+++ Program.cs
@@ -3,17 +3,18 @@ int[] arrayInteiros = new int[3];
 
 arrayInteiros[0] = 10;
 arrayInteiros[1] = 20;
-arrayInteiros[2] = 30;
+arrayInteiros[2] = int.Parse("30");
+arrayInteiros[3] = 40;
 
-System.Console.WriteLine("Percorrenco o array pelo for");
+// System.Console.WriteLine("Percorrenco o array pelo for");
 
-for (int i = 0; i < arrayInteiros.Length; i++)
-{
-    System.Console.WriteLine(arrayInteiros[i]);
-}
+// for (int i = 0; i < arrayInteiros.Length; i++)
+// {
+//     System.Console.WriteLine(arrayInteiros[i]);
+// }
 
-System.Console.WriteLine("Percorrendo o array pelo foreach");
-foreach (var intem in arrayInteiros)
-{
-    System.Console.WriteLine(intem);
-}
+// System.Console.WriteLine("Percorrendo o array pelo foreach");
+// foreach (var intem in arrayInteiros)
+// {
+//     System.Console.WriteLine(intem);
+// }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Fundamentos: Coleções e LINQ com .NET6
 ## Praticando com a plataforma [DIO](https://web.dio.me)
 
-#### Agora com C#10 e .NET6 ficou tudo mais enchuto, agilizando na hora de escrever suas linha de código.
+#### Agora com C#10 e .NET6 ficou tudo mais enxuto, agilizando na hora de escrever suas linha de código.
 
 #### Saiba mais nesse link abaixo:
 [Gerando Modelos com C#10 Instruções de Nível Superior](https://aka.ms/new-console-template)
